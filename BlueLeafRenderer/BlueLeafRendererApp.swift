d9b5486cb1470ec6a8a12e8aef9022942bc5f845
Sahil Srivastava <previouslysahil@gmail.com>
2022-11-23 21:09:59 -0500

    Initial Commit

---
+++ BlueLeafRenderer/BlueLeafRendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueLeafRendererApp.swift
+//  BlueLeafRenderer
+//
+//  Created by Sahil Srivastava on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BlueLeafRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
